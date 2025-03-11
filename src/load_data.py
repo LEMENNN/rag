a80d9e07903ade815f947f823abc4d8ecde858be
LEMENNN <ceetbana@gmail.com>
2025-03-11 22:56:01 +0100

    correction

--- src/load_data.py
+++ src/load_data.py
@@ -15,13 +15,6 @@ def load_documents():
     loader = PyPDFDirectoryLoader("bdc/")  # Charge tous les PDF d'un dossier
     documents = loader.load()
 
-
-    # for file in os.listdir(BDC_PATH):
-    #      if file.endswith(".pdf"):
-    #          loader = PyPDFLoader(file)
-    #          documents.extend(loader.load())
-    # print(f"✅ {len(documents)} documents chargé")
-
     return documents
 
 
@@ -29,15 +22,5 @@ def load_documents():
 
 
 
-# texts = load_document()
-
-# chunks = split_text([doc.page_content for doc in texts], chunk_size=200, chunk_overlap=20)
-
-
-# # Afficher les résultats
-# for i, chunk in enumerate(chunks[:5]):  # Afficher les 5 premiers chunks
-#     print(f"Chunk {i+1}:\n{chunk}\n{'-'*50}")
-
-# print(f"Total chunks générés : {len(chunks)}")
 
 
